0247fb660341ff0e7d3adbfff352ab6878086b82
Petar-Antov <soulflybg@gmail.com>
2025-09-10 17:28:33 +0300

    Create clean&load_silver.crm_sales_detials.sql

---
+++ scripts/sliver/clean&load_silver.crm_sales_detials.sql
@@ -0,0 +1,14 @@
+If OBJECT_ID  ('silver.crm_sales_details','U')  is not null
+    drop table silver.crm_sales_details;
+CREATE TABLE [silver].[crm_sales_details](
+	[sls_ord_num] [nvarchar](50) NULL,
+	[sls_prd_key] [nvarchar](50) NULL,
+	[sls_cust_id] [int] NULL,
+	[sls_order_dt] [date] NULL,
+	[sls_ship_dt] [date] NULL,
+	[sls_due_dt] [date] NULL,
+	[sls_sales] [int] NULL,
+	[sls_quantity] [int] NULL,
+	[sls_price] [int] NULL,
+	dwh_create_date datetime2 default getdate()
+) 
