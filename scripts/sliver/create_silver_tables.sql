7738d7cf69e77a1f20ef33fce4b234e8ecbd5656
Petar-Antov <soulflybg@gmail.com>
2025-09-10 17:28:04 +0300

    Update create_silver_tables.sql

--- scripts/sliver/create_silver_tables.sql
+++ scripts/sliver/create_silver_tables.sql
@@ -33,15 +33,14 @@ CREATE TABLE [silver].[crm_sales_details](
 	[sls_ord_num] [nvarchar](50) NULL,
 	[sls_prd_key] [nvarchar](50) NULL,
 	[sls_cust_id] [int] NULL,
-	[sls_order_dt] [int] NULL,
-	[sls_ship_dt] [int] NULL,
-	[sls_due_dt] [int] NULL,
+	[sls_order_dt] [date] NULL,
+	[sls_ship_dt] [date] NULL,
+	[sls_due_dt] [date] NULL,
 	[sls_sales] [int] NULL,
 	[sls_quantity] [int] NULL,
 	[sls_price] [int] NULL,
 	dwh_create_date datetime2 default getdate()
 ) 
-
 If OBJECT_ID  ('silver.erp_cust_az12','U')  is not null
     drop table silver.erp_cust_az12;
 CREATE TABLE [silver].[erp_cust_az12](
