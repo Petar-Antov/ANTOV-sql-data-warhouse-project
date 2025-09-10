2541eabe9348f6ec9097bc29792f1de79b56325c
Petar-Antov <soulflybg@gmail.com>
2025-09-10 11:37:42 +0300

    Create create_silver_tables.sql

---
+++ scripts/sliver/create_silver_tables.sql
@@ -0,0 +1,62 @@
+
+If OBJECT_ID  ('silver.crm_cust_info','U')  is not null
+    drop table silver.crm_cust_info;
+CREATE TABLE [silver].[crm_cust_info](
+	[cst_id] [int] NULL,
+	[cst_key] [nvarchar](50) NULL,
+	[cst_firstname] [nvarchar](50) NULL,
+	[cst_lastname] [nvarchar](50) NULL,
+	[cst_marital_status] [nvarchar](50) NULL,
+	[cst_gndr] [nvarchar](50) NULL,
+	[cst_create_date] [date] NULL
+) 
+
+If OBJECT_ID  ('silver.crm_prd_info','U')  is not null
+    drop table silver.crm_prd_info;
+CREATE TABLE [silver].[crm_prd_info](
+	[prd_id] [int] NULL,
+	[prd_key] [nvarchar](50) NULL,
+	[prd_nm] [nvarchar](50) NULL,
+	[prd_cost] [int] NULL,
+	[prd_line] [nvarchar](50) NULL,
+	[prd_start_dt] [datetime] NULL,
+	[prd_end_dt] [datetime] NULL
+)
+
+If OBJECT_ID  ('silver.crm_sales_details','U')  is not null
+    drop table silver.crm_sales_details;
+CREATE TABLE [silver].[crm_sales_details](
+	[sls_ord_num] [nvarchar](50) NULL,
+	[sls_prd_key] [nvarchar](50) NULL,
+	[sls_cust_id] [int] NULL,
+	[sls_order_dt] [int] NULL,
+	[sls_ship_dt] [int] NULL,
+	[sls_due_dt] [int] NULL,
+	[sls_sales] [int] NULL,
+	[sls_quantity] [int] NULL,
+	[sls_price] [int] NULL
+) 
+
+If OBJECT_ID  ('silver.erp_cust_az12','U')  is not null
+    drop table silver.erp_cust_az12;
+CREATE TABLE [silver].[erp_cust_az12](
+	[CID] [nvarchar](50) NULL,
+	[BDAT] [date] NULL,
+	[GEN] [nvarchar](50) NULL
+) 
+
+If OBJECT_ID  ('silver.erp_loc_a101','U')  is not null
+    drop table silver.erp_loc_a101;
+CREATE TABLE [silver].[erp_loc_a101](
+	[CID] [nvarchar](50) NULL,
+	[CNTRY] [nvarchar](50) NULL
+) 
+
+If OBJECT_ID  ('silver.erp_px_cat_g1v2','U')  is not null
+    drop table silver.eerp_px_cat_g1v2;
+CREATE TABLE [silver].[erp_px_cat_g1v2](
+	[ID] [nvarchar](50) NULL,
+	[CAT] [nvarchar](50) NULL,
+	[SUBCAT] [nvarchar](50) NULL,
+	[MAINTENANCE] [nvarchar](50) NULL
+) 
