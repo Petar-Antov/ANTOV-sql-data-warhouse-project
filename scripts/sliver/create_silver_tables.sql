b92ca415df5e4f53e97f328533ce37b032711f92
Petar-Antov <soulflybg@gmail.com>
2025-09-10 11:41:23 +0300

    Update create_silver_tables.sql

--- scripts/sliver/create_silver_tables.sql
+++ scripts/sliver/create_silver_tables.sql
@@ -8,7 +8,8 @@ CREATE TABLE [silver].[crm_cust_info](
 	[cst_lastname] [nvarchar](50) NULL,
 	[cst_marital_status] [nvarchar](50) NULL,
 	[cst_gndr] [nvarchar](50) NULL,
-	[cst_create_date] [date] NULL
+	[cst_create_date] [date] NULL,
+	dwh_create_date datetime2 default getdate()
 ) 
 
 If OBJECT_ID  ('silver.crm_prd_info','U')  is not null
@@ -20,7 +21,8 @@ CREATE TABLE [silver].[crm_prd_info](
 	[prd_cost] [int] NULL,
 	[prd_line] [nvarchar](50) NULL,
 	[prd_start_dt] [datetime] NULL,
-	[prd_end_dt] [datetime] NULL
+	[prd_end_dt] [datetime] NULL,
+	dwh_create_date datetime2 default getdate()
 )
 
 If OBJECT_ID  ('silver.crm_sales_details','U')  is not null
@@ -34,7 +36,8 @@ CREATE TABLE [silver].[crm_sales_details](
 	[sls_due_dt] [int] NULL,
 	[sls_sales] [int] NULL,
 	[sls_quantity] [int] NULL,
-	[sls_price] [int] NULL
+	[sls_price] [int] NULL,
+	dwh_create_date datetime2 default getdate()
 ) 
 
 If OBJECT_ID  ('silver.erp_cust_az12','U')  is not null
@@ -42,21 +45,24 @@ If OBJECT_ID  ('silver.erp_cust_az12','U')  is not null
 CREATE TABLE [silver].[erp_cust_az12](
 	[CID] [nvarchar](50) NULL,
 	[BDAT] [date] NULL,
-	[GEN] [nvarchar](50) NULL
+	[GEN] [nvarchar](50) NULL,
+	dwh_create_date datetime2 default getdate()
 ) 
 
 If OBJECT_ID  ('silver.erp_loc_a101','U')  is not null
     drop table silver.erp_loc_a101;
 CREATE TABLE [silver].[erp_loc_a101](
 	[CID] [nvarchar](50) NULL,
-	[CNTRY] [nvarchar](50) NULL
+	[CNTRY] [nvarchar](50) NULL,
+	dwh_create_date datetime2 default getdate()
 ) 
 
 If OBJECT_ID  ('silver.erp_px_cat_g1v2','U')  is not null
-    drop table silver.eerp_px_cat_g1v2;
+    drop table silver.erp_px_cat_g1v2;
 CREATE TABLE [silver].[erp_px_cat_g1v2](
 	[ID] [nvarchar](50) NULL,
 	[CAT] [nvarchar](50) NULL,
 	[SUBCAT] [nvarchar](50) NULL,
-	[MAINTENANCE] [nvarchar](50) NULL
+	[MAINTENANCE] [nvarchar](50) NULL,
+	dwh_create_date datetime2 default getdate()
 ) 
