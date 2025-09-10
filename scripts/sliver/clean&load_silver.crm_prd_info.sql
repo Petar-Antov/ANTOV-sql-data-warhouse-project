50fce8f90f4595adc2e8e19c25312fa1ec36cea2
Petar-Antov <soulflybg@gmail.com>
2025-09-10 16:02:40 +0300

    Create clean&load_silver.crm_prd_info.sql

---
+++ scripts/sliver/clean&load_silver.crm_prd_info.sql
@@ -0,0 +1,27 @@
+insert into silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt)
+
+select 
+prd_id,
+replace(substring(prd_key,1,5),'-','_') as cat_id,
+substring(prd_key,7,len(prd_key)) as prd_key,
+prd_nm,
+isnull(prd_cost,0) as prd_cost,
+case upper(trim(prd_line))
+     when 'M' then 'Mountian'
+     when 'R' then 'Road'
+     when 'S' then 'Other Sales'
+     when 'T' then 'Touring'
+     else 'n/a'
+end as prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over (partition by prd_key order by prd_start_dt asc) - 1 as date) as prd_end_dt
+
+from bronze.crm_prd_info
